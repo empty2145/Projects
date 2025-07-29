6999013e2b1bf07969331c6a6b1ed2e5a0a8f893
TamirDev <tasybaevtamir@gmail.com>
2025-07-29 23:05:32 +0500

    Create automated tests for formatCurrency

--- JavaScript-FulCourse/javascript-amazon-project-main/scripts/checkout/orderSummary.js
+++ JavaScript-FulCourse/javascript-amazon-project-main/scripts/checkout/orderSummary.js
@@ -3,7 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js"
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption, calculateDeliveryDate } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
 import { renderCheckoutHeader } from "./checkoutHeader.js";
 
@@ -18,14 +18,7 @@ export function renderOrderSummary() {
         const deliveryOptionId = cartItem.deliveryOptionId;
 
         const deliveryOption = getDeliveryOption(deliveryOptionId);
-        const today = dayjs();
-        const deliveryDate = today.add(
-            deliveryOption.deliveryDays, 'days'
-
-        );
-        const dateString = deliveryDate.format(
-            'dddd, MMMM D'
-        );
+        let dateString = calculateDeliveryDate(deliveryOption);
 
         cartSummaryHTML += `
             <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -74,14 +67,7 @@ export function renderOrderSummary() {
     function deliveryOptionsHTML(matchingProduct, cartItem) {
         let html = '';
         deliveryOptions.forEach((deliveryOption) => {
-            const today = dayjs();
-            const deliveryDate = today.add(
-                deliveryOption.deliveryDays, 'days'
-
-            );
-            const dateString = deliveryDate.format(
-                'dddd, MMMM D'
-            );
+            let dateString = calculateDeliveryDate(deliveryOption);
 
             const priceString = deliveryOption.priceCents === 0
                 ? 'FREE'
